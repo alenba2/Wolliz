7cb475939db1366a3196ab760ae07f7488188b8d
alenba2 <baekallen@gmail.com>
2023-03-18 23:19:23 -0700

    Created Router and deleted some files related to React

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import LoginPage from './Login_SignUp/LoginPage'
+import SignUpPage from './Login_SignUp/SignUpPage'
+import SellerPage from './SellerHouse/SellerPage'
+import UserListingPage from './UserListings/UserListingPage'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Routes>
+      {/* On Boot */}
+      <Route path='/' exact element={<LoginPage />} />
+      {/* Other Site */}
+
+      <Route path='/SignUp' element={<SignUpPage />} />
+      <Route path='/SellerPage' element={<SellerPage />} />
+      <Route path='/UserListing' element={<UserListingPage />} />
+
+      {/* Error Case */}
+      {/* <Route element={<LoginPage />} /> */}
+    </Routes>
+  )
 }
 
-export default App;
+export default App
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import LoginPage from './Login_SignUp/LoginPage'
-import SignUpPage from './Login_SignUp/SignUpPage'
-import SellerPage from './SellerHouse/SellerPage'
-import UserListingPage from './UserListings/UserListingPage'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
 import reportWebVitals from './reportWebVitals'
 
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
@@ -12,7 +10,9 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <UserListingPage />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 )
 
